19428f7be46d38c016f007783ed532d7533bfcc0
nealpatel03 <neal@captionpartners.com>
2022-06-07 16:49:56 -0500

    Added print_name

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_name('Bob')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def print_name(name):
+    print(name)
